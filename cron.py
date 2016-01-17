a109345b4b9d2c19c9e332c873bcf9db4a86b622
Shobhit Mittal <shobhitmittal250@gmail.com>
2016-01-17 23:28:42 +0530

    added a fb page to cron

--- cron.py
+++ cron.py
@@ -3,6 +3,6 @@ import time
 
 while True:
 	time.sleep(500)
-	url='http://128.199.82.223/apiv1_fb_cron?twitter_source=Bloodhelpline,iCanSaveLife,BloodAid&fb_source=Blooddonors,bloodnetwork,BLOODFORSUREapp,RaktAbhaa'
+	url='http://128.199.82.223/apiv1_fb_cron?twitter_source=Bloodhelpline,iCanSaveLife,BloodAid&fb_source=Blooddonors,Plateletdonors,bloodnetwork,BLOODFORSUREapp,RaktAbhaa'
 	status_request= requests.get(url)
 	time.sleep(3000)
