2c9081c379e49221a9f19d1f6a4902599f1096ac
Shobhit Mittal <shobhitmittal250@gmail.com>
2016-01-17 23:38:01 +0530

    added status return in run file

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 nohup /usr/bin/python cron.py >/dev/null 2>&1 &
+
+if [ `ps aux |grep -v grep | grep -c cron.py` -gt 0 ]; then
+
+echo 'Success in Running cron script'
+
 cd kookoo_hack/
 nohup /usr/bin/python manage.py runserver 0.0.0.0:80 >/dev/null 2>&1 &
+
+if [ `ps aux | grep -v grep| grep -c manage.py` -gt 0 ]; then
+
+echo 'The Server is running succesfully.You can logout now.'
